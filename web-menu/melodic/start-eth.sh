d4cfb803f98506b7da160e59b6b2ee4b6f8cc8a0
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2020-06-19 15:35:00 +0400

    update web-menu linux service

--- web-menu/melodic/start-eth.sh
+++ web-menu/melodic/start-eth.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /opt/ros/melodic/setup.bash
+source /home/ubuntu/geoscan_ws/build/setup.bash
 python3 /home/ubuntu/web-menu/web-eth-menu.py
--- web-menu/noetic/start-eth.sh
+++ web-menu/noetic/start-eth.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
+source /home/ubuntu/geoscan_ws/build/setup.bash
 python3 /home/ubuntu/web-menu/web-eth-menu.py
