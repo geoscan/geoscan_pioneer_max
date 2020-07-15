c1b03fd79d88fc2e9b8c0401959a3ee206a0a22f
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2020-07-15 22:16:42 +0400

    lua table

--- lua/test_pioneer_uart.lua
+++ lua/test_pioneer_uart.lua
@@ -32,52 +32,183 @@ local function color(r,g,b)
     end
 end
 
-local function split(s, delimiter)
-    result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match)
+local function lcolor(r,g,b) 
+    for i=4,28,1 do
+        leds:set(i,r,g,b)
     end
-    return result
+end
+
+local function write_msg(msg)
+    msg="&" .. msg .. "#"
+    uart:write(msg,#msg)
+end
+
+local function convert(num)
+    if(num==nil) then 
+        return 0.0
+    else
+        return num
+    end
 end
 
 local events={
+    ["0"]=function()
+        ap.push(Ev.MCE_PREFLIGHT)
+        write_msg("prfl")
+    end,
     ["1"]=function()
-        
+        ap.push(Ev.MCE_TAKEOFF)
+        write_msg("tkff")
     end,
     ["2"]=function()
-    end,
-    ["3"]=function()
+        ap.push(Ev.MCE_LANDING)
+        write_msg("land")
     end
 }
 
 local command={
     ["evnt"]=function(data)
-        local x=string.unpack("> I",data)
-        if(x==1) then
-            color(1,1,1)
-            local msg=string.pack("> I",#data)
-            uart:write(msg,#msg)
-        end
+       local x=data:sub(1,1)
+       events[x]()
+    end,
+    ["gtlp"]=function(data)
+        local x=0.0
+        local y=0.0
+        local z=0.0
+        local t=0.0
+        x,y,z,t=string.unpack("> f f f f",data)
+        ap.goToLocalPoint(x,y,z,t)
+        write_msg("gtlp")
+    end,
+    ["gtpg"]= function(data)
+        local latitude= 0.0
+        local longitude = 0.0
+        local altitude = 0.0
+        latitude,longitude,altitude=string.unpack("> f f f",data)
+        ap.goToPoint(latitude,longitude.altitude)
+        write_msg("gtpg")
+    end,
+    ["updy"]= function(data)
+        local yaw=0.0
+        yaw=string.unpack("> f",data)
+        ap.updateYaw(yaw)
+        write_msg("updy")
+    end,
+    ["bled"]=function(data)
+        local n=0
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        n,r,g,b=string.unpack("> I1 f f f",data)
+        leds:set(n,r,g,b)
+        write_msg("bled")
+    end,
+    ["aled"]=function(data)
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        color(r,g,b)
+        write_msg("aled")
+    end,
+    ["mled"]=function(data)
+        local n=0
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        n,r,g,b=string.unpack("> I1 f f f")
+        leds:set(n,r,g,b)
+        write_msg("mled")
+    end,
+    ["lled"]=function(data)
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        lcolor(r,g,b)
+        write_msg("lled")
+    end,
+    ["time"]=function(data)
+        write_msg(string.pack("> s4 f","time",time()))
+    end,
+    ['dltm']=function(data)
+        write_msg(string.pack("> s4 f","dltm",deltaTime()))
+    end,
+    ["lntm"]=function(data)
+        write_msg(string.pack("> s4 f","lntm",launchTime()))
+    end,
+    ["bnum"]=function(data)
+        write_msg(string.pack("> s4 s","bnum",boardNumber))
+    end,
+    ["lpsp"]=function(data)
+        local lpsX=0.0
+        local lpsY=0.0
+        local lpsZ=0.0
+        lpsX, lpsY, lpsZ = lpsPosition()
+        write_msg(string.pack("> s4 f f f","lpsp",convert(lpsX),convert(lpsY),convert(lpsZ)))
+    end,
+    ["lpsv"]=function(data)
+        local lpsVelX=0.0
+        local lpsVelY=0.0
+        local lpsVelZ=0.0
+        lpsVelX, lpsVelY, lpsVelZ = lpsVelocity()
+        write_msg(string.pack("> s4 f f f","lpsv",convert(lpsVelX),convert(lpsVelY),convert(lpsVelZ)))
+    end,
+    ["lpsy"]=function(data)
+        local lpsYaw=0.0
+        lpsYaw=lpsYaw()
+        write_msg(string.pack("> s4 f","lpsy",convert(lpsYaw)))
+    end,
+    ["ornt"]=function(data)
+        local roll=0.0
+        local pitch=0.0
+        local azimuth =0.0
+        roll, pitch, azimuth = orientation()
+        write_msg(string.pack("> s4 f f f","ornt",roll,pitch,azimuth))
+    end,
+    ["accl"]=function(data)
+        local ax=0.0
+        local ay=0.0
+        local az=0.0
+        ax, ay, az = accel()
+        write_msg(string.pack("> s4 f f f","accl",ax,ay,az))
+    end,
+    ["gyro"]=function(data)
+        local gx=0.0
+        local gy=0.0
+        local gz=0.0
+        gx, gy, gz = gyro()
+        write_msg(string.pack("> s4 f f f","gyro",gx,gy,gz))
+    end,
+    ["rnge"]=function(data)
+        local r1=0.0
+        local r2=0.0
+        local r3=0.0
+        local r4=0.0
+        local r5=0.0
+        d1,d2,d3,d4,d5=range()
+        write_msg(string.pack("> s4 f f f f f","rnge",convert(r1),convert(r2),convert(r3),convert(r4),convert(r5)))
+    end,
+    ["altd"]=function(data)
+        local h=0.0
+        h=altitude()
+        write_msg(string.pack("> s4 f","altd",h))
+    end,
+    ["strt"]=function(data)
+        write_msg("ok")
+        color(0,0,0)
+        lcolor(0,0,0)
     end
 }
 
 local function logic(data)
-    st={}
-    st=split(data,"-")
-    color(0,0,1)
-    if(#st~=0) then
-        local code=st[1]
-        color(1,0,0)
-        command[code](st[2])
-    end
+    code=data:sub(1,4)
+    command[code](data:sub(4,#data))
 end
-
 
 local function takeFunc()
     inp = uart:read(1)
     if is_taking then 
         if inp == "&" then
-            for n = 0, #packet, 1 do
+            for n = 0, #packet, 1 do 
                 toWrite = toWrite .. packet[n]
             end
             is_taking = false
@@ -97,10 +228,19 @@ end
 
 
 function callback(event)
+    if(event == Ev.ENGINES_STARTED)then
+        write_msg("egst")
+    elseif(event == Ev.COPTER_LANDED)then
+        write_msg("crld")
+    elseif(event == Ev.TAKEOFF_COMPLETE)then
+        write_msg("ptrd")
+    elseif(event == Ev.POINT_REACHED)then
+        write_msg("ptrd")
+    end
 end
 
-color(0,0,0)
-
 t = Timer.new(sync, takeFunc)
+write_msg("okp")
 color(0,0,0)
+lcolor(0,0,0)
 t:start()
