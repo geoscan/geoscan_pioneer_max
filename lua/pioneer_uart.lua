b47436d63c8edb9c5b88bc316f1aa86f4b0f40cd
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2020-07-20 14:04:39 +0400

    Update pioneer_uart.lua

--- lua/pioneer_uart.lua
+++ lua/pioneer_uart.lua
@@ -1,5 +1,5 @@
-local boardNumber = "1"
-local firmwareVersion = "2.0/cfg"
+local boardNumber = 4
+local firmwareVersion = "3.0/cfg"
 
 local uartNum = 1
 local baudRate = 9600 
@@ -12,9 +12,7 @@ local leds=Ledbar.new(29)
 local sync = 0.001
 local is_taking = false
 local inp = ''
-local j = 0
-local toWrite = ""
-local packet = {}
+local data = ""
 
 local lpsPosition = Sensors.lpsPosition
 local lpsVelocity = Sensors.lpsVelocity
@@ -25,7 +23,6 @@ local range = Sensors.range
 local accel = Sensors.accel
 local rc = Sensors.rc
 local altitude = Sensors.altitude
-
 
 local function color(r,g,b) 
     for i=0,3,1 do
@@ -39,174 +36,202 @@ local function lcolor(r,g,b)
     end
 end
 
-local function split(s, delimiter)
-    result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
-    end
-    return result;
-end
-
-local function convert(dt)
-    dts=tostring(dt)
-    if (dts=="nil")then
-        return "0.0"
-    else
-        l=false
-        o=0
-        msg=""
-        for i = 1,#dts,1 do
-            c=dts:sub(i,i)
-            msg=msg .. c
-            if(c==".") then
-                l=true
-            end
-            if(l==true) then
-                o=o+1
-            end
-            if(o==4) then
-                break
-            end
-        end
-        return msg
-    end
-end
-
 local function write_msg(msg)
     msg="&" .. msg .. "#"
     uart:write(msg,#msg)
 end
 
-local function logic(data)
-    st={}
-    st=split(data,"-")
-    if(#st~=0) then
-        local code = st[1]
-        if(code=="start") then
-            write_msg("ok")
-            color(0,0,0)
-            lcolor(0,0,0)
-        elseif(code=="led") then
-            leds:set(st[2],tonumber(st[3]),tonumber(st[4]),tonumber(st[5]))
-            write_msg("led")
-        elseif(code=="aled") then
-            color(tonumber(st[2]),tonumber(st[3]),tonumber(st[4]))
-            write_msg("aled")
-        elseif(code=="lled") then
-            leds:set(st[2]+4,tonumber(st[3]),tonumber(st[4]),tonumber(st[5]))
-            write_msg("lled")
-        elseif(code=="laled") then
-            lcolor(tonumber(st[2]),tonumber(st[3]),tonumber(st[4]))
-            write_msg("laled")
-        elseif(code=="time")then
-            write_msg(convert(time()))
-        elseif(code=="dltm")then
-            write_msg(convert(deltaTime()))
-        elseif(code=="lntm")then
-            write_msg(convert(launchTime()))
-        elseif(code=="bnum")then
-            write_msg(tostring(boardNumber))
-        elseif(code=="eve")then
-            local evente = st[2]
-            if(evente == "0")then
-                ap.push(Ev.MCE_PREFLIGHT)
-                write_msg("pre")
-            elseif(evente == "1")then
-                ap.push(Ev.MCE_TAKEOFF)
-                write_msg("tkff")
-            elseif(evente == "2")then
-                ap.push(Ev.MCE_LANDING)
-                write_msg("land")
-            else
-                write_msg("event_err")
-            end
-        elseif(code=="gtlp")then
-            local x = tonumber(st[2])
-            local y = tonumber(st[3])
-            local z = tonumber(st[4])
-            local times = tonumber(st[5])
-            if(times==0)then
-                ap.goToLocalPoint(x,y,z)
-                write_msg("gtlp")
-            else
-                ap.goToLocalPoint(x,y,z,times)
-                write_msg("gtlp")
-            end
-        elseif(code=="gtp")then
-            local latitude=tonumber(st[2])
-            local longitude=tonumber(st[3])
-            local altitude=tonumber(st[4])
-            ap.goToPoint(latitude,longitude,altitude)
-            write_msg("gtp")
-        elseif(code=="updy")then
-            local yaw = tonumber(st[2])
-            ap.updateYaw(yaw)
-            write_msg("updy")
-        elseif(code=="lpspos")then
-            lpsX, lpsY, lpsZ = lpsPosition()
-            write_msg("lpspos:" .. convert(lpsX) .. ":" .. convert(lpsY) .. ":" .. convert(lpsZ))
-        elseif(code=="lpsvel")then
-            lpsVelX, lpsVelY, lpsVelZ = lpsVelocity()
-            write_msg("lpsvel:" .. convert(lpsVelX) .. ":" .. convert(lpsVelY) .. ":" .. convert(lpsVelZ))
-        elseif(code=="lpsyaw")then
-            yaw = lpsYaw()
-            write_msg("lpsyaw:" .. convert(yaw))
-        elseif(code=="ort")then
-            roll, pitch, azimuth = orientation()
-            write_msg("ort:" .. convert(roll) .. ":" .. convert(pitch) .. ":" .. convert(azimuth))
-        elseif(code=="acl")then
-            ax, ay, az = accel()
-            write_msg("acl:" .. convert(ax) .. ":" .. convert(ay) .. ":" .. convert(az))
-        elseif(code=="gyro")then
-            gx, gy, gz = gyro()
-            write_msg("gyro:" .. convert(gx) .. ":" .. convert(gy) .. ":" .. convert(gz))
-        elseif(code=="range")then
-            d1,d2,d3,d4,d5=range()
-            write_msg("range:" .. convert(d1)..":" .. convert(d2).. ":" .. convert(d3).. ":" .. convert(d4).. ":" .. convert(d5))
-        elseif(code=="alt")then
-            h=altitude()
-            write_msg("alt:" .. convert(h))
-        end
+local function convert(num)
+    if(num==nil) then 
+        return 0.0
+    else
+        return num
     end
 end
+
+local events={
+    ["prfl"]=function()
+        ap.push(Ev.MCE_PREFLIGHT)
+        write_msg(string.pack("> c4","prfl"))
+    end,
+    ["tkff"]=function()
+        ap.push(Ev.MCE_TAKEOFF)
+        write_msg(string.pack("> c4","tkff"))
+    end,
+    ["land"]=function()
+        ap.push(Ev.MCE_LANDING)
+        write_msg(string.pack("> c4","land"))
+    end
+}
+
+local command={
+    ["evnt"]=function(data)
+       local x=data:sub(1,4)
+       if ((x=="1") or (x=="2") or (x=="0")) then
+           events[x]()
+       else
+           write_msg(string.pack("> c4","ever"))
+       end
+    end,
+    ["gtlp"]=function(data)
+        local x=0.0
+        local y=0.0
+        local z=0.0
+        local t=0.0
+        x,y,z,t=string.unpack("> f f f f",data)
+        ap.goToLocalPoint(x,y,z,t)
+        write_msg(string.pack("> c4","gtlp"))
+    end,
+    ["gtpg"]= function(data)
+        local latitude= 0.0
+        local longitude = 0.0
+        local altitude = 0.0
+        latitude,longitude,altitude=string.unpack("> f f f",data)
+        ap.goToPoint(latitude,longitude.altitude)
+        write_msg(string.pack("> c4","gtpg"))
+    end,
+    ["updy"]= function(data)
+        local yaw=0.0
+        yaw=string.unpack("> f",data)
+        ap.updateYaw(yaw)
+        write_msg(string.pack("> c4","updy"))
+    end,
+    ["bled"]=function(data)
+        local n=0
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        n,r,g,b=string.unpack("> I1 f f f",data)
+        leds:set(n,r,g,b)
+        write_msg(string.pack("> c4","bled"))
+    end,
+    ["aled"]=function(data)
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        r,g,b=string.unpack("> f f f", data)
+        color(r,g,b)
+        write_msg(string.pack("> c4","aled"))
+    end,
+    ["mled"]=function(data)
+        local n=0
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        n,r,g,b=string.unpack("> I1 f f f",data)
+        leds:set(n,r,g,b)
+        write_msg(string.pack("> c4","mled"))
+    end,
+    ["lled"]=function(data)
+        local r=0.0
+        local g=0.0
+        local b=0.0
+        r,g,b=string.unpack("> f f f", data)
+        lcolor(r,g,b)
+        write_msg(string.pack("> c4","lled"))
+    end,
+    ["time"]=function(data)
+        write_msg(string.pack("> c4 f","time",time()))
+    end,
+    ['dltm']=function(data)
+        write_msg(string.pack("> c4 f","dltm",deltaTime()))
+    end,
+    ["lntm"]=function(data)
+        write_msg(string.pack("> c4 f","lntm",launchTime()))
+    end,
+    ["bnum"]=function(data)
+        write_msg(string.pack("> c4 I1","bnum",boardNumber))
+    end,
+    ["lpsp"]=function(data)
+        local lpsX=0.0
+        local lpsY=0.0
+        local lpsZ=0.0
+        lpsX, lpsY, lpsZ = lpsPosition()
+        write_msg(string.pack("> c4 f f f","lpsp",convert(lpsX),convert(lpsY),convert(lpsZ)))
+    end,
+    ["lpsv"]=function(data)
+        local lpsVelX=0.0
+        local lpsVelY=0.0
+        local lpsVelZ=0.0
+        lpsVelX, lpsVelY, lpsVelZ = lpsVelocity()
+        write_msg(string.pack("> c4 f f f","lpsv",convert(lpsVelX),convert(lpsVelY),convert(lpsVelZ)))
+    end,
+    ["lpsy"]=function(data)
+        local lpsYaw=0.0
+        lpsYaw=lpsYaw()
+        write_msg(string.pack("> c4 f","lpsy",convert(lpsYaw)))
+    end,
+    ["ornt"]=function(data)
+        local roll=0.0
+        local pitch=0.0
+        local azimuth =0.0
+        roll, pitch, azimuth = orientation()
+        write_msg(string.pack("> c4 f f f","ornt",roll,pitch,azimuth))
+    end,
+    ["accl"]=function(data)
+        local ax=0.0
+        local ay=0.0
+        local az=0.0
+        ax, ay, az = accel()
+        write_msg(string.pack("> c4 f f f","accl",ax,ay,az))
+    end,
+    ["gyro"]=function(data)
+        local gx=0.0
+        local gy=0.0
+        local gz=0.0
+        gx, gy, gz = gyro()
+        write_msg(string.pack("> c4 f f f","gyro",gx,gy,gz))
+    end,
+    ["rnge"]=function(data)
+        local r1=0.0
+        local r2=0.0
+        local r3=0.0
+        local r4=0.0
+        local r5=0.0
+        d1,d2,d3,d4,d5=range()
+        write_msg(string.pack("> c4 f f f f f","rnge",convert(r1),convert(r2),convert(r3),convert(r4),convert(r5)))
+    end,
+    ["altd"]=function(data)
+        local h=0.0
+        h=altitude()
+        write_msg(string.pack("> c4 f","altd",h))
+    end,
+    ["strt"]=function(data)
+        write_msg(string.pack("> c2","oks"))
+        color(0,0,0)
+        lcolor(0,0,0)
+    end
+}
 
 local function takeFunc()
     inp = uart:read(1)
     if is_taking then 
         if inp == "&" then
-            for n = 0, #packet, 1 do 
-                toWrite = toWrite .. packet[n]
-            end
             is_taking = false
-            logic(toWrite)
-            toWrite = ""
-            packet={}
-            j = 0
-            
+            command[data:sub(1,4)](data:sub(5,#data))
+            data = ""
         else
-            packet[j] = inp 
-            j=j+1
+            data = data .. inp
         end
     elseif(inp == "#")then
         is_taking = true
     end
 end
 
-
 function callback(event)
     if(event == Ev.ENGINES_STARTED)then
-        write_msg("dbg-engst")
+        write_msg(string.pack("> c4","egst"))
     elseif(event == Ev.COPTER_LANDED)then
-        write_msg("dbg-cprlnd")
+        write_msg(string.pack("> c4","crld"))
     elseif(event == Ev.TAKEOFF_COMPLETE)then
-        write_msg("dbg-pntrchd")
+        write_msg(string.pack("> c4","ptrd"))
     elseif(event == Ev.POINT_REACHED)then
-        write_msg("dbg-pntrchd")
+        write_msg(string.pack("> c4","ptrd"))
     end
 end
 
 t = Timer.new(sync, takeFunc)
-write_msg("okp")
+write_msg(string.pack("> c3","okp"))
 color(0,0,0)
-lcolor(0,0,0) 
+lcolor(0,0,0)
 t:start()
